23b56798a9d8418b716e84b073b8e5d713abf302
Gleb Osetrin <glebbest5@gmail.com>
2025-05-30 02:55:28 +0500

    fixed part 3 Osetrin_homework_4

--- homework/GlebOsetrin/homework4/my_dictionary.py
+++ homework/GlebOsetrin/homework4/my_dictionary.py
@@ -10,12 +10,13 @@ Set = {4, 8, 12, 16.5, 'very_fun', True}
 Set.add('song')
 Set.remove(12)
 
-Dict ={'My name': 'Gleb',
+Dict =  {
+        'My name': 'Gleb',
         'My surname': 'Osetrin',
         'My age': '30',
         'My job': 'QA Engineer',
         'My hobby': 'swim'
-     }
+        }
 
 Dict[('i am a tuple',)] = 'New word'
 Dict.pop('My hobby')
