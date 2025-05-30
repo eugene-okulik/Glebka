9d6c9105e28960d167170d337cbad49c0bb8c662
Gleb Osetrin <glebbest5@gmail.com>
2025-05-30 17:22:25 +0500

    fixed my mistakes on my homework4

--- homework/GlebOsetrin/homework4/my_dictionary.py
+++ homework/GlebOsetrin/homework4/my_dictionary.py
@@ -1,25 +1,25 @@
 
-List = [1, 5, 15, 28, 'brother', 'sister', 'dog', 25.5, 'cat', 'Yes', False]
-List.append('tatos')
-List.pop(1)
+list1 = [1, 5, 15, 28, 'brother', 'sister', 'dog', 25.5, 'cat', 'Yes', False]
+list1.append('tatos')
+list1.pop(1)
 
-Tuple = (10, 20, 30, 'frog', 'shark')
-print(Tuple[-1])
+tuple1 = (10, 20, 30, 'frog', 'shark')
+print(tuple1[-1])
 
-Set = {4, 8, 12, 16.5, 'very_fun', True}
-Set.add('song')
-Set.remove(12)
+set1 = {4, 8, 12, 16.5, 'very_fun', True}
+set1.add('song')
+set1.remove(12)
 
-Dict =  {
-        'My name': 'Gleb',
-        'My surname': 'Osetrin',
-        'My age': '30',
-        'My job': 'QA Engineer',
-        'My hobby': 'swim'
-        }
+dict1 = {
+    'My name': 'Gleb',
+    'My surname': 'Osetrin',
+    'My age': '30',
+    'My job': 'QA Engineer',
+    'My hobby': 'swim'
+}
 
-Dict[('i am a tuple',)] = 'New word'
-Dict.pop('My hobby')
+dict1[('i am a tuple',)] = 'New word'
+dict1.pop('My hobby')
 
-mydict = {'list': List, 'tuple': Tuple, 'set': Set, 'dict': Dict}
+mydict = {'list': list1, 'tuple': tuple1, 'set': set1, 'dict': dict1}
 print(mydict)
