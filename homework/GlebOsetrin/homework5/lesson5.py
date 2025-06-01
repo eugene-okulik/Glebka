ca8044c0b0ddd08ee05958b2147197b8ae9438cc
Gleb Osetrin <glebbest5@gmail.com>
2025-06-02 00:34:12 +0500

    help me please with 1 task:(

--- homework/GlebOsetrin/homework5/lesson5.py
+++ homework/GlebOsetrin/homework5/lesson5.py
@@ -5,21 +5,21 @@ city = 'New York'
 phone = '+1372829383739'
 country = 'US'
 # я не понял первое задание :( перевел дословно и сделал дословно
-
+# и есть ли разница в питоне между " и '
 result1 = 'результат операции: 42'
 result2 = 'результат операции: 514'
 result3 = 'результат работы программы: 9'
-colon_position1 = result1.index(":")
+colon_position1 = result1.index(':')
 number1_str = result1[colon_position1+2:]
 number1 = int(number1_str)
 print(number1 + 10)
 
-colon_position2 = result2.index(":")
+colon_position2 = result2.index(':')
 number2_str = result2[colon_position2+2:]
 number2 = int(number2_str)
 print(number2 + 10)
 
-colon_position3 = result3.index(":")
+colon_position3 = result3.index(':')
 number3_str = result3[colon_position3+2:]
 number3 = int(number3_str)
 print(number3 + 10)
