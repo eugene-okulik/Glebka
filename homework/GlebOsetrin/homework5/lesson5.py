9b4e1c5273179e60e9430c061f8686c187cebdc3
Gleb Osetrin <glebbest5@gmail.com>
2025-06-02 00:43:13 +0500

    добавил пробелы между знаком сложения

--- homework/GlebOsetrin/homework5/lesson5.py
+++ homework/GlebOsetrin/homework5/lesson5.py
@@ -10,17 +10,17 @@ result1 = 'результат операции: 42'
 result2 = 'результат операции: 514'
 result3 = 'результат работы программы: 9'
 colon_position1 = result1.index(':')
-number1_str = result1[colon_position1+2:]
+number1_str = result1[colon_position1 + 2:]
 number1 = int(number1_str)
 print(number1 + 10)
 
 colon_position2 = result2.index(':')
-number2_str = result2[colon_position2+2:]
+number2_str = result2[colon_position2 + 2:]
 number2 = int(number2_str)
 print(number2 + 10)
 
 colon_position3 = result3.index(':')
-number3_str = result3[colon_position3+2:]
+number3_str = result3[colon_position3 + 2:]
 number3 = int(number3_str)
 print(number3 + 10)
 
