fd1d6b98e1f35ad5279a5220a3ee212e93ca0602
Gleb Osetrin <glebbest5@gmail.com>
2026-01-22 22:41:10 +0500

    osetrin_homework_lesson3

---
+++ homeworknew/homework3/osetrin_homework3_task1.py
@@ -0,0 +1,5 @@
+a = 9
+b = 4
+print(a + b)
+print(a - b)
+print(a * b)
---
+++ homeworknew/homework3/osetrin_homework3_task3.py
@@ -0,0 +1,8 @@
+c = 3
+d = 9
+
+a = (c + d) / 2         # a =  c + d / 2
+g = (c * d) ** 0.5      # g = корень c * d
+
+print(a)
+print(g)
