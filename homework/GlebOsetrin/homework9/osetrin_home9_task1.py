c856e9a452a0fca4e60e418abcfa8cd191a65dbb
Gleb Osetrin <glebbest5@gmail.com>
2026-02-13 00:38:07 +0500

    homework9_osetrin

---
+++ homework/GlebOsetrin/homework9/osetrin_home9_task1.py
@@ -0,0 +1,8 @@
+import datetime
+my_date = 'Jan 15, 2023 - 12:05:33'
+python_date = datetime.datetime.strptime(my_date,'%b %d, %Y - %H:%M:%S')
+my_month = python_date.strftime('%B')
+my_fulldate = python_date.strftime ('%d.%m.%Y %H:%M')
+
+print(my_month)
+print(my_fulldate)
